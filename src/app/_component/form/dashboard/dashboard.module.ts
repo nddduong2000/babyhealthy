a8c8ebd1e4d68940c85a044696cd759d65e82334
DuongND <ndduong2000@gmail.com>
2023-08-08 16:25:59 +0700

    commit

--- src/app/_component/form/dashboard/dashboard.module.ts
+++ src/app/_component/form/dashboard/dashboard.module.ts
@@ -5,18 +5,27 @@ import { DashboardRoutingModule } from './dashboard-routing.module';
 import { HeaderDashboardComponent } from './header-dashboard/header-dashboard.component';
 import { BannerComponent } from './banner/banner.component';
 import { IntroduceComponent } from './introduce/introduce.component';
+import { ArticleComponent } from './article/article.component';
+import { ExploreComponent } from './explore/explore.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
   declarations: [
     HeaderDashboardComponent,
     BannerComponent,
-    IntroduceComponent
+    IntroduceComponent,
+    ArticleComponent,
+    ExploreComponent,
+    FooterComponent,
   ],
   exports: [
     HeaderDashboardComponent,
     BannerComponent,
-    IntroduceComponent
+    IntroduceComponent,
+    ArticleComponent,
+    ExploreComponent,
+    FooterComponent,
   ],
   imports: [
     CommonModule,
